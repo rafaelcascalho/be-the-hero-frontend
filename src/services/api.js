0b63450b6aaa3220a8d40415af54fabfc5d90ebd
= <=>
2020-04-12 12:25:52 -0300

    hotfix: added env vars support to axios and modified netlify toml file

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+function getBaseUrl() {
+  if (process.env.NODE_ENV === 'production') {
+    return process.env.REACT_APP_API_URL;
+  }
+  return 'http://localhost:4223/api/v1';
+}
 
 const api = axios.create({
-  baseURL: 'http://localhost:4223/api/v1',
+  baseURL: getBaseUrl(),
+  responseType: 'json',
 });
 
 export default api;
